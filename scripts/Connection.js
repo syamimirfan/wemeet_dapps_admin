ea8f54ce121c72d90cd81f87d76a569df3e3a3aa
Muhamad Syamim Irfan Bin Ahmad Shokkri <syamimirfan59@gmail.com>
2023-06-12 16:15:39 +0800

    Backend development

--- scripts/Connection.js
+++
@@ -1,7 +0,0 @@
-class Connection {
-    ADMIN_ACCOUNT = process.env.ADMIN_ACCOUNT;
-    TOKEN_ADDRESS = process.env.TOKEN_ADDRESS;
-    PRIVATE_KEY = process.env.PRIVATE_KEY;
-    INFURA_SEPOLIA_ENDPOINT = process.env.INFURA_SEPOLIA_ENDPOINT;
-    CONTRACT_ABI = process.env.CONTRACT_ABI;
-}
