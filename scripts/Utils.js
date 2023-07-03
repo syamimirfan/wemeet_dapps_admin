ac311134eef604ec0afd3a096b97412f806d47f1
Muhamad Syamim Irfan Bin Ahmad Shokkri <syamimirfan59@gmail.com>
2023-07-03 22:01:22 +0800

    Updated file image only

--- scripts/Utils.js
+++ scripts/Utils.js
@@ -1,6 +1,6 @@
 class Utils {
      //port from backend (node.js)
      //deployed from render
-     // baseURL = "http://localhost:5000";
-     baseURL = "https://wemeet-dapps-backend.onrender.com";
+      baseURL = "http://localhost:5000";
+     //baseURL = "https://wemeet-dapps-backend.onrender.com";
 }
